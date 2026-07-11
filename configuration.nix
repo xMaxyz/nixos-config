5be221469f641abc67e484a9755aaba9dc2f9a17
Max Goedecke <max_goedecke@gmx.de>
2026-07-12 00:20:47 +0200

    new flake attempt

--- configuration.nix
+++ configuration.nix
@@ -101,6 +101,10 @@
         persist = true;
       }];
     };
+    
+    sudo = {
+      enable = true;
+    };
   };
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "NixOS Flake Config";
   
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
   };
 
   outputs = { self, nixpkgs, ... }@inputs: {
