5faa823fd461c659dede21e561e2e412fa080f6a
Max Goedecke <max_goedecke@gmx.de>
2026-07-18 20:49:04 +0200

    vim auto-close brackets and auto-indent

---
+++ home/vim/vim.nix
@@ -0,0 +1,47 @@
+{ pkgs, config, ... }:
+
+{
+  programs.vim = {
+    enable = true;
+    settings = {
+      number = true;
+      tabstop = 2;
+      shiftwidth = 2;
+      expandtab = true;
+    };
+
+    extraConfig = ''
+      set nocompatible
+
+      filetype on
+      filetype plugin on
+      filetype indent on
+
+      syntax on
+      set cursorline
+
+      set smartcase
+      set showmode
+      set hlsearch
+
+      set autoindent
+      set smartindent
+      filetype plugin indent on
+
+      " Mappings
+      inoremap jk <Esc>
+
+      inoremap ( ()<Left>
+      inoremap [ []<Left>
+      inoremap { {}<Left>
+      inoremap ' \'''\'<Left>
+      inoremap \'''\' \'''\'\'<Left><Left>
+      inoremap " ""<Left>
+
+      inoremap (<CR> (<CR>)<Esc>O
+      inoremap [<CR> [<CR>]<Esc>O
+      inoremap {<CR> {<CR>}<Esc>O
+      inoremap \'''\'<CR> \'''\'<CR>\'''\'<Esc>O
+    '';
+  };
+}
