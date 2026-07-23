2a1ff44285ef69cece713743e6ac5fc8717fd23e
Max Goedecke <max_goedecke@gmx.de>
2026-07-23 13:39:46 +0200

    added fstrim

--- modules/configuration.nix
+++ modules/configuration.nix
@@ -102,6 +102,7 @@
   # Enable the OpenSSH daemon.
   services.openssh.enable = true;
   services.upower.enable = true;
+  services.fstrim.enable = true;
 
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
