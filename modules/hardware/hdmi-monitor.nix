f6b1e13768cadf1bd6afca06a5bf19c39ce048e4
Max Goedecke <max_goedecke@gmx.de>
2026-07-27 19:05:26 +0200

    fixed type error in hdmi-monitor.nix

--- modules/hardware/hdmi-monitor.nix
+++ modules/hardware/hdmi-monitor.nix
@@ -3,7 +3,11 @@
 {
   home-manager.users.max.programs.niri.settings.outputs = {
     "HDMI-A-1" = {
-      mode = "1920x1080@60.000";
+      mode = {
+        width = 1920;
+        height = 1080;
+        refresh = 60.000;
+      };
       position = { x = 1920; y = 0; };
     };
   };
