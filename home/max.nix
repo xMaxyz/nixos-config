f362a30d2f38c15ca5f927b3832bd79b5f586386
Max Goedecke <max_goedecke@gmx.de>
2026-08-30 10:43:14 +0200

    max.nix: replaced home.sessionVariables with xdg.enable and added comments

--- home/max.nix
+++ home/max.nix
@@ -1,11 +1,18 @@
 { config, pkgs, inputs, host, ... }:
 
 {
+  #modules to be inserted into HM-scope.
   imports = 
   [
+    #imports the theme for this user. Don't change this.
     ./max/current-theme.nix
+    
+    #imports all the packages user-wide.
     ./max/programs.nix
     
+    #all the files in ./max/
+    #these are the configs for the "desktop" programs.
+    #leaving one of these out will result in the program using its default settings.
     ./max/zsh.nix
     ./max/git.nix
     ./max/fuzzel.nix
@@ -18,19 +25,23 @@
     ./max/kitty.nix
   ];
   
+  #basic HM settings.
+  #There is absolutely no reason to change this at all.
   home.username = "max";
   home.homeDirectory = "/home/max";
   home.stateVersion = "26.05";
 
+  #enables the XDG-module that automatically sets all the XDG variables
+  #home.sessionVariables seems to be deprecated.
+  xdg.enable = true;
+
+  #PATH extensions
   home.sessionPath = [
     "$HOME/.local/bin"
   ];
 
-  home.sessionVariables = {
-    XDG_CONFIG_HOME = "$HOME/.config";
-    XDG_CACHE_HOME = "$HOME/.cache";
-  };
-
+  #enables home manager. 
+  #Do NOT turn this off as it would break user-scope for this user!
   programs = {
     home-manager.enable = true;
   };
